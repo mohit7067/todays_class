74bfd6a14fe40f9df35722dc76bda8e44b9c872a
Mohit Patil <97459016+mohit7067@users.noreply.github.com>
2022-08-21 01:10:12 +0530

    done

---
+++ index.js
@@ -0,0 +1,26 @@
+let arr= [
+    {
+      "name":"sid",
+      "age":23,
+      "location":"naded",
+      "nationality":"Indian"
+    },
+    {
+      "name":"sid",
+      "age":23,
+      "location":"naded",
+      "nationality":"Indian"
+    },
+    {
+      "name":"sid",
+      "age":23,
+      "location":"naded",
+      "nationality":"Indian"
+    },
+    {
+      "name":"sid",
+      "age":23,
+      "location":"naded",
+      "nationality":"Indian"
+    },
+]
